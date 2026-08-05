7761b5085679bfe08be9f1afedcff12dde6129bf
marciocaldeira86 <caldeira_cristiano@hotmail.com>
2026-08-05 17:32:38 -0300

    Add files via upload

---
+++ arquivo/arquivo.py
@@ -0,0 +1,10 @@
+with open("teste.txt", "w") as pasta1:
+    pasta1.write("primeiro texto criado em arquivo")
+
+with open("clientes.txt", "w") as financeiro:
+   financeiro.write("lista de clientes:\n")
+   financeiro.write("cliente 01\n")
+   financeiro.write("cliente 02\n")
+   financeiro.write("cliente 03\n")
+   
+   
